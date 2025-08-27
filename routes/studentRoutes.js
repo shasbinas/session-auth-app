f7d816398e8a3b1852069406cc8ca64efa01f178
shasbinas <shasbinas1@gmail.com>
2025-08-27 22:21:25 +0530

    studentrouteadded

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,1 +1,9 @@
+import express from "express";
+import { showStudents } from "../controllers/studentController.js";
+import { isAuthenticated } from "../middleware/authMiddleware.js";
 
+const router = express.Router();
+
+router.get("/", isAuthenticated, showStudents);
+
+export default router;
