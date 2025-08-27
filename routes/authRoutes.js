1b717510e052155363a3192d61ea21d83685c9c2
shasbinas <shasbinas1@gmail.com>
2025-08-27 22:16:34 +0530

    authroutes added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,11 @@
 
+import express from "express";
+import { showProducts, addProduct } from "../controllers/productController.js";
+import { isAuthenticated } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", isAuthenticated, showProducts);
+router.post("/add", isAuthenticated, addProduct);
+
+export default router;
