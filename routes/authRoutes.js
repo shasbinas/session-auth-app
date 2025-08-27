b1e42941d9fc6425552d5a96e801cf7c046cc59a
shasbinas <shasbinas1@gmail.com>
2025-08-27 23:00:28 +0530

    authroute login issue solved

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ import { showLogin, login, logout } from "../controllers/authController.js";
 
 const router = express.Router();
 
+router.get("/login", showLogin);
 router.get("/", showLogin);
 router.post("/login", login);
 router.get("/logout", logout);
